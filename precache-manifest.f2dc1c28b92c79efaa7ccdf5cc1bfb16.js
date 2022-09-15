41ef96e1599e5998157b6cbe17854540cff77a40
Abhishek <sorenabhishek.as@gmail.com>
2022-09-16 03:48:50 +0530

    Updates

---
+++ precache-manifest.f2dc1c28b92c79efaa7ccdf5cc1bfb16.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "fa0bc380d39b6fe40126",
+    "url": "/netflix-clone/static/css/main.2a1e9a5a.chunk.css"
+  },
+  {
+    "revision": "fa0bc380d39b6fe40126",
+    "url": "/netflix-clone/static/js/main.fa0bc380.chunk.js"
+  },
+  {
+    "revision": "8f1f9bdd8571ce5e7bb7",
+    "url": "/netflix-clone/static/js/1.8f1f9bdd.chunk.js"
+  },
+  {
+    "revision": "b9e0979d65f9fb8c90c1",
+    "url": "/netflix-clone/static/js/2.b9e0979d.chunk.js"
+  },
+  {
+    "revision": "9d2f2c861f62b52b32dd",
+    "url": "/netflix-clone/static/js/runtime~main.9d2f2c86.js"
+  },
+  {
+    "revision": "63aa52ecd5b86d81116d9ebb270ab87c",
+    "url": "/netflix-clone/static/media/back.63aa52ec.jpg"
+  },
+  {
+    "revision": "cf7cb627f43ad311ad50b72f21c193f5",
+    "url": "/netflix-clone/static/media/part1.cf7cb627.png"
+  },
+  {
+    "revision": "a48333d43c19612ed61987fa5dbff3c5",
+    "url": "/netflix-clone/static/media/part2.a48333d4.jpg"
+  },
+  {
+    "revision": "6c66a29acbadd2fd15b177f8671c8d37",
+    "url": "/netflix-clone/static/media/part3.6c66a29a.png"
+  },
+  {
+    "revision": "d2cb38e3d5bb9c1428e058ff99328ae7",
+    "url": "/netflix-clone/static/media/part4.d2cb38e3.png"
+  },
+  {
+    "revision": "f59cb764d35815f258a28ea9196605d5",
+    "url": "/netflix-clone/index.html"
+  }
+];
